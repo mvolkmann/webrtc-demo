7a68ccab7eafec8b0708719a927155ef5af12327
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-05-17 11:02:42 -0500

    progress

--- client/src/lib/Room.svelte
+++ client/src/lib/Room.svelte
@@ -1,11 +1,27 @@
 <script>
+  // @ts-nocheck
+
   import Icon from 'fa-svelte';
-  import {faDoorClosed} from '@fortawesome/free-solid-svg-icons';
+  import {
+    faComments,
+    faDesktop,
+    faDoorClosed,
+    faHandPaper,
+    faMicrophone,
+    faVideo
+  } from '@fortawesome/free-solid-svg-icons';
   import {createEventDispatcher} from 'svelte';
   import {currentRoom} from './stores.js';
 
   const dispatch = createEventDispatcher();
+
+  let audioOn = false;
+  let chat = false;
+  let handRaised = false;
+  let screenSharingOn = false;
+  let shareScreen = false;
   let videoGrid;
+  let videoOn = false;
 
 </script>
 
@@ -21,10 +37,51 @@
     </button>
   </h2>
 
+  <div class="buttons">
+    <button
+      class={'bare' + (audioOn ? '' : ' off')}
+      title="turn on audio"
+      on:click={() => (audioOn = !audioOn)}
+    >
+      <Icon icon={faMicrophone} />
+    </button>
+    <button
+      class={'bare' + (videoOn ? '' : ' off')}
+      title="turn on video"
+      on:click={() => (videoOn = !videoOn)}
+    >
+      <Icon icon={faVideo} />
+    </button>
+    <button
+      class={'bare' + (shareScreen ? '' : ' off')}
+      title="share screen"
+      on:click={() => (shareScreen = !shareScreen)}
+    >
+      <Icon icon={faDesktop} />
+    </button>
+    <button
+      class={'bare' + (chat ? '' : ' off')}
+      title="show chat"
+      on:click={() => (chat = !chat)}
+    >
+      <Icon icon={faComments} />
+    </button>
+    <button
+      class={'bare' + (handRaised ? '' : ' off')}
+      title="raise hand"
+      on:click={() => (handRaised = !handRaised)}
+    >
+      <Icon icon={faHandPaper} />
+    </button>
+  </div>
+
   <div id="video-grid" bind:this={videoGrid} />
 </section>
 
 <style>
+  button.off :global(.fa-svelte) {
+    color: lightgray;
+  }
   h2 {
     display: flex;
     align-items: center;
